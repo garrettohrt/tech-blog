a758a41dd07b0d53eea6efffcb6257ad19c31a85
gmun <garrettohrt@gmail.com>
2022-12-08 14:44:38 -0600

    finish userRoutes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -58,47 +58,91 @@ router.post('/', async (req, res) => {
             req.session.user_id = dbUserData.id;
             req.session.username = dbUserData.username;
             req.session.loggedIn = true;
-            
+
             res.status(200).json(userData);
         })
 
-        
+
     } catch (err) {
         res.status(400).json({ message: 'failed to create user' })
     }
 });
 
 router.post('/signup', async (req, res) => {
+    var newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
 
-    try {
-        const userData = await User.create(req.body);
-
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(400).json({ message: 'failed to create user' })
-    }
+    await User.findOne({
+        where: {
+            username: req.body.username,
+            email: req.body.email
+        }
+    }).then(async profile => {
+        if (!profile) {
+            await newUser.save()
+                .then(() => {
+                    res.status(200).send(newUser);
+                })
+                .catch(err => {
+                    console.log("Error is ", err.message);
+                });
+        } else {
+            res.send("User already exists...");
+        }
+    }).catch(err => {
+        console.log("Error is", err.message);
+    });
 });
 
 router.post('/login', async (req, res) => {
 
     try {
-        const userData = await User.create(req.body);
+        const userData = await User.findOne({
+            where: {
+                username: req.body.username
+            }
+        });
 
-        res.status(200).json(userData);
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+
     } catch (err) {
-        res.status(400).json(err)
+        res.status(400).json(err);
     }
 });
 
-router.post('/logout', async (req, res) => {
 
-    try {
-        const userData = await User.create(req.body);
-
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(400).json(err)
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
     }
-});
+  });
 
 module.exports = router;
